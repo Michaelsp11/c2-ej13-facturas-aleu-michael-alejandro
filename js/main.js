a2f16f589e94f7cd79c850e8df4da84da6b3464a
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-01 19:06:32 +0200

    Se generan las facturas y agregados num,fecha y concepto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+import { facturas } from "../datos/facturas.js";
+
 const getIva = (porcentajeIVA, base) => (porcentajeIVA / 100) * base;
+const parsearFecha = (timestamp) => new Date(timestamp).toLocaleDateString();
+const listaFacturas = document.querySelector(".lista-facturas");
+const pintarFacturaTabla = () => {
+    for (const { numero, fecha, concepto }
+        of facturas) {
+        const nuevaFactura = document
+            .querySelector(".factura-molde")
+            .cloneNode(true);
+        nuevaFactura.classList.remove("factura-molde");
+        nuevaFactura.classList.remove("d-none");
+        const numeroFactura = nuevaFactura.querySelector(".numero");
+        numeroFactura.textContent = numero;
+        const fechaFactura = nuevaFactura.querySelector(".fecha");
+        fechaFactura.textContent = parsearFecha(fecha);
+        const conceptoFactura = nuevaFactura.querySelector(".concepto");
+        conceptoFactura.textContent = concepto;
+        listaFacturas.append(nuevaFactura);
+    }
+};
+pintarFacturaTabla();
