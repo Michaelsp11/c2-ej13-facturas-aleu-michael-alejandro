042298f976dae0b6a178beeb419fa19a7613c2e5
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-01 19:23:55 +0200

    Base y el total de los bases ya se pintan en la vista

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const getIva = (porcentajeIVA, base) => (porcentajeIVA / 100) * base;
 const parsearFecha = (timestamp) => new Date(timestamp).toLocaleDateString();
 const total = (base, iva) => base + iva;
 const colorFondo = (celda, boolean) =>
-  boolean
-    ? celda.classList.add("table-success")
-    : celda.classList.add("table-danger");
+    boolean ?
+    celda.classList.add("table-success") :
+    celda.classList.add("table-danger");
 const listaFacturas = document.querySelector(".lista-facturas");
 const pintarFacturaTabla = () => {
-    for (const { numero, fecha, concepto }
+    for (const { numero, fecha, concepto, base }
         of facturas) {
         const nuevaFactura = document
             .querySelector(".factura-molde")
@@ -22,7 +22,17 @@ const pintarFacturaTabla = () => {
         fechaFactura.textContent = parsearFecha(fecha);
         const conceptoFactura = nuevaFactura.querySelector(".concepto");
         conceptoFactura.textContent = concepto;
-        listaFacturas.append(nuevaFactura);
+        const baseFactura = nuevaFactura.querySelector(".base");
+        baseFactura.textContent = base;
+        const totalBaseFactura = listaFacturas.append(nuevaFactura);
     }
 };
+const pintarTotalBase = () => {
+    const totalBase = document.querySelector(".total-base");
+    totalBase.textContent = facturas.reduce(
+        (acumulador, { base }) => acumulador + base,
+        0
+    );
+};
 pintarFacturaTabla();
+pintarTotalBase();
