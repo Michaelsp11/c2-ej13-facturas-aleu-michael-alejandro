729ec67f0d326f142ce2e838e7f35fca8344b5d7
aleupons <apahoquei@gmail.com>
2021-06-01 19:20:22 +0200

    Función para la columna vence (factura abonada, fechas, color celda...)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const getIva = (porcentajeIVA, base) => (porcentajeIVA / 100) * base;
 const total = (base, iva) => base + iva;
+const compararFechas = () => true;
 const colorFondo = (celda, boolean) =>
   boolean
     ? celda.classList.add("table-success")
     : celda.classList.add("table-danger");
+const vence = (celda, vencimiento, abonada, callback) => {
+  if (abonada) {
+    celda.textContent = "-";
+    callback(celda, abonada);
+  } else {
+    celda.textContent = vencimiento.toLocaleString();
+    if (vencimiento < Date.getTime()) {
+      celda.textContent = compararFechas();
+      callback(celda, false);
+    } else {
+      celda.textContent = compararFechas();
+      callback(celda, true);
+    }
+  }
+};
